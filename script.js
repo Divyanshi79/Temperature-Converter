9308b5c4636a8bed2921ca664e63a36010a069d0
Divyanshi79 <58603015+Divyanshi79@users.noreply.github.com>
2021-04-12 08:04:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const calculateTemp = () => {
+    const numberTemp = document.getElementById('temp').value;
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+    let result;
+
+    const celToFah = (cel) => {
+        let fahrenheit = Math.round((cel * 9/5) + 32);
+        return fahrenheit;
+    }
+
+    const fahToCel = (fah) => {
+        let celcius = Math.round((fah-32)*5/9);
+        return celcius;
+    }
+    if(valueTemp == 'cel'){
+        result = celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result}°Fahrenheit`;
+    }
+    else{
+        result = fahToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result}°Celcius`;
+    }
+}
